f1f1aaccef84c50e636dc7681a30e0ea4c47730f
Paco Ferre <paco@debiandev.pacoferre.com>
2018-09-10 22:55:12 +0200

    Rinkeby semi-configuration

--- ethereum-the-airline/truffle-config.js
+++ ethereum-the-airline/truffle-config.js
@@ -1,10 +1,17 @@
+const HDWalletProvider = request('truffle-hdwallet-provider');
+const mnemonic = 'words';
+
 module.exports = {
   networks: {
-    development: {      
+    development: {
       host: 'localhost',
       port: 7545,
       network_id: '*',
       gas: 5000000
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, "https://infura..."),
+      network_id: '4'
     }
   }
 }
